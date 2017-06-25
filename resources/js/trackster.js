c7cc86fb032ba56e543c418fdca5ac771ccf7e95
Anshul889 <anshulmanaktala@gmail.com>
2017-06-26 01:58:02 +0530

    Update trackster.js

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,18 +1,16 @@
 var Trackster = {};
-
+const API_KEY = 'dcf6bb7698a9340d3778f125954e8ae1';
 $(document).ready(function() {
   $('#search-button').click(function() {
     Trackster.searchTracksByTitle($('#search-input').val());
   });
 });
-
 Trackster.renderTracks = function(tracks) {
   var $trackList = $('#track-list');
-
   $trackList.empty();
-
   for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
     var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
     var htmlTrackRow =
       '<div class="row track">' +
       '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
@@ -21,23 +19,20 @@ Trackster.renderTracks = function(tracks) {
       '    </a>' +
       '  </div>' +
       '  <div class="col-xs-4">' + track.name + '</div>' +
-      '  <div class="col-xs-2">' + track.artists[0].name + '</div>' +
-      '  <div class="col-xs-2">' + track.album.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artist + '</div>' +
+      // '  <div class="col-xs-2">' + track.album + '</div>' +
+      '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
       '  <div class="col-xs-2">' + track.popularity + '</div>' +
       '</div>';
-
     $trackList.append(htmlTrackRow);
   }
 };
 
-const API_KEY = 'dcf6bb7698a9340d3778f125954e8ae1';
-
 Trackster.searchTracksByTitle = function(title) {
   $.ajax({
-    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=' + API_KEY + title,
-    datatype: 'jsonp',
+    url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track= " + title + "&api_key=" + API_KEY+ '&format=json',
     success: function(response) {
-      Trackster.renderTracks(response.tracks.items);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   });
 };
